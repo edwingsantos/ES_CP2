e7f846487874c5470642e132c26c0fbf483d0b2c
edwingsantos <edwing.santos@ucas-edu.net>
2026-01-08 09:52:08 -0700

    ldkjfa;lkdjf

---
+++ Individual_projects/RPG_manager.py
@@ -0,0 +1,12 @@
+#ES 1 RPG manageer prject 
+
+
+
+
+#Make a while true loop
+
+# ask the user what the goal is as an input as a varable called goals
+#if goal is a digit make it equal True. And ineger goal >0
+#then print nice goal. And break
+#else print please select an option that is over zero and is a number
+
