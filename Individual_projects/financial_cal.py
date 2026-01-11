aa0ea9c362c22e86541a4fb844739027f93685a2
edwingsantos <edwing.santos@ucas-edu.net>
2026-01-11 10:12:56 -0700

    asdfgh

--- Individual_projects/financial_cal.py
+++ Individual_projects/financial_cal.py
@@ -33,5 +33,22 @@ def goal_reach():
             break
         else:
             continue
-goal_reach()
+
 #test,variable = check(variable)
+while True:
+    print("Welcome to your financial calculator")
+    choice= input("Enter the number so select an option \n " 
+    "1. Savings Time Calculator \n" 
+    "2. Compound Interest Calculator \n" 
+    "3. Budget Allocator \n"
+    "4. Sale Price Calculato \n"
+    "5. Tip Calculator\n")
+    if choice.isdigit == True:
+        if choice == 1:
+            goal_reach()
+        elif choice == 2:
+            print("hi")
+    else:
+        print(";alksd ")
+        continue
+    
