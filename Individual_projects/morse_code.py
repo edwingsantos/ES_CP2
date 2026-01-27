4d955e442549bfa2189095c9d406d83019f0d9fc
edwingsantos <edwing.santos@ucas-edu.net>
2026-01-27 09:51:48 -0700

    started spedococe for my morse code thing

---
+++ Individual_projects/morse_code.py
@@ -0,0 +1,24 @@
+#ES 1rst morse code translator
+
+
+
+
+#make a funtion for morse to english
+    #print the alphabet in morse code, and put the letter 
+
+#make a funtion from english to morse
+
+
+#make a funtion for main menu
+
+    #make a while loop
+
+        #make the user input the option of where they want to go, name it choice
+
+        #if choice is 1. call morse_to_english
+
+        #if choice is 2. call english to morse
+
+        #if choice is 3. print that they have decided to leave, break
+
+        #else print to select an acutal option, continue
