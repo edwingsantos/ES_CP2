05b90896e9d07f597b0e059d50e638e1de0da02e
edwingsantos <edwing.santos@ucas-edu.net>
2026-02-27 09:51:51 -0700

    started with fractal

---
+++ Individual_projects/fractal.py
@@ -0,0 +1,45 @@
+#ES 1st fractal pattern generator
+
+#import turtle
+import turtle
+
+#make a funtion for fractiorial and have depth, color and backround parameter in it 
+    #if depth equals 1:return 1
+    #for i in range(3)
+        #turtle forward lentgh
+        #turtle turn 60 degress 
+    #return i time factorial depth -1
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#make a funtion for main and put depth. color and backround as parameters
+    #print the greeting for the code
+    #make a while loop 
+        #make depth be an input the recursion depth 
+
+        #make color be a input of what color they want their triangle to be
+
+        #make backround be an input if they want to change the backround color
+
+
+#call main
